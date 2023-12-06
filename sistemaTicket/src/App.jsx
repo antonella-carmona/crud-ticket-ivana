cc806ff5eb38be9b2b4a4aa1a77e3088973a1022
Ivana-Antonella <antonellacarmona090@gmail.com>
2023-12-06 17:26:14 -0300

    header

---
+++ sistemaTicket/src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+import NavBar from "./components/NavBar"
+
+
+
+function App() {
+
+
+  return (
+    <main className='px-4 pt-6'>
+      <NavBar/>
+    </main>
+  )
+}
+
+export default App
---
+++ sistemaTicket/src/components/NavBar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import burgerMenu from "../assets/icon-menu.svg";
+
+const NavBar = () => {
+  return (
+    <header className="flex place-content-between items-center  bg-gray-300 p-4">
+        
+      <div>
+        <h1 className="w-35 h-38 font-bold">Configuración para tickets</h1>
+      </div>
+
+
+      <ul className='hidden sm:flex text-[18px] sm:w-[438px] sm:place-content-around sm:text-[16px] sm:items-center'>
+
+        <li><a href="#">Ayuda</a></li>
+        <li><a href="#">Registro de Errores</a></li>
+        <li><a href="#">Procesos</a></li>
+        <li><a href="#">Login usuario</a></li>
+
+      </ul>
+
+      <img className='w-10 h-4 cursor-pointer sm:hidden' src={burgerMenu} alt="Menu amburguesa" />
+
+    </header>
+  );
+}
+
+export default NavBar;  // Usa punto y coma en lugar de coma aquí
