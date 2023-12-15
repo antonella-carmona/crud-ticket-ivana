210e28d95520fb2aeff29dd1030eb0434ca46512
Ivana-Antonella <antonellacarmona090@gmail.com>
2023-12-15 17:54:53 -0300

    mini servidor

--- sistemaTicket/src/components/Search.jsx
+++ sistemaTicket/src/components/Search.jsx
@@ -57,7 +57,7 @@ const Search = () => {
           type="search"
           id="search-dropdown"
           className="block p-2.5 w-80 z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg rounded-s-gray-100 rounded-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
-          placeholder="Search"
+          placeholder="Buscar por título"
           value={search}
           onChange={handleName}
         />
