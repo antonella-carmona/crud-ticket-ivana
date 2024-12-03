196e31abb0013a69eda5ccb9290bf1ff954fb2be
Ivana-Antonella <antonellacarmona090@gmail.com>
2024-12-03 14:22:05 -0300

    gfds

--- servidor/src/controllers/createTickets.js
+++ servidor/src/controllers/createTickets.js
@@ -2,17 +2,44 @@ const { Ticket } = require("../db");
 
 
 const createTickets = async (título, descripción, estado, categoría, date) => {
-     
-    const newTicket = await Ticket.create({
-      título,
-      descripción,
-      estado,
-      categoría,
-      date,
-    });
+  //  typescript
+  // interface TicketParams {
+  //   título: string;
+  //   descripción: string;
+  //   estado: string;
+  //   categoría: string;
+  //   date: Date;
+  // }
 
-  return newTicket
-    
+  // Definimos la función utilizando el tipo
+  // const createTickets = async ({
+  //   título,
+  //   descripción,
+  //   estado,
+  //   categoría,
+  //   date,
+  // }: TicketParams): Promise<typeof Ticket> => {
+  //   const newTicket = await Ticket.create({
+  //     título,
+  //     descripción,
+  //     estado,
+  //     categoría,
+  //     date,
+  //   });
+
+  //   return newTicket;
+  // };
+
+  // JavaScript 
+  const newTicket = await Ticket.create({
+    título,
+    descripción,
+    estado,
+    categoría,
+    date,
+  });
+
+  return newTicket;
 };
 
 module.exports = {
